857ed651f01c88f9a1dbeb364e53b1e4a694bf90
18211167516 <18211167516@163.com>
2021-10-31 16:05:37 +0800

    middleware Recovery

--- app/middleware/recovery.go
+++ app/middleware/recovery.go
@@ -2,10 +2,14 @@ package middleware
 
 import (
 	"fmt"
+	"net"
+	"net/http/httputil"
+	"os"
 	"runtime"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 
 	"go-api/global"
 	"go-api/tool"
@@ -30,9 +34,36 @@ func Recovery() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if err := recover(); err != nil {
+				// Check for a broken connection, as it is not really a
+				// condition that warrants a panic stack trace.
+				var brokenPipe bool
+				if ne, ok := err.(*net.OpError); ok {
+					if se, ok := ne.Err.(*os.SyscallError); ok {
+						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") || strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
+							brokenPipe = true
+						}
+					}
+				}
+
 				message := trace(fmt.Sprintf("%s", err))
 				//string(debug.Stack())
-				global.LOG.Error(message)
+				httpRequest, _ := httputil.DumpRequest(c.Request, false)
+				if brokenPipe {
+					global.LOG.Error(c.Request.URL.Path,
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+					)
+					// If the connection is dead, we can't write a status to it.
+					_ = c.Error(err.(error)) // nolint: errcheck
+					c.Abort()
+					return
+				} else {
+					global.LOG.Error("[Recovery from panic]",
+						zap.Any("error", err),
+						zap.String("request", string(httpRequest)),
+						zap.String("key string", message),
+					)
+				}
 
 				if global.VP.GetString("RunMode") == "release" {
 					message = "Internal Server Error"
